6c784dfb622ac97c797613631b17bfc085e8a636
Bianca Grecea <grecea.bianca@gmail.com>
2020-11-15 20:55:39 +0200

    Added tema_2

--- tema-1/src/Main.java
+++ tema-1/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int steps = 15;
+        int steps = 30;
         JsonLoader jsonLoader = new JsonLoader();
 
         if(jsonLoader.LoadJson("src/files/InputFile.json")){
